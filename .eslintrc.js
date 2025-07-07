e26231d0555bcc1ab2af0da5eccc98fcbcc4dd63
lhp2901 <lehoangphuc29011986@gmail.com>
2025-07-07 07:34:54 +0700

    🚫 fix: shut down eslint completely for build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  extends: [],
-  rules: {},
-  ignorePatterns: ['**/*'], // ✅ Bỏ qua toàn bộ project khi build
+  extends: ['next/core-web-vitals'], // hoặc 'next' nếu không dùng web-vitals
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+  },
 }
